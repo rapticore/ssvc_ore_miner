59eea055a66d1a653a086e79a410abe4a40981ea
Azeem Irshad <azeem.irshad@rapticore.com>
2023-05-15 14:30:18 +0500

    commented out environment variables which are not required

--- rapticoressvc/test/test_data_helper.py
+++ rapticoressvc/test/test_data_helper.py
@@ -8,10 +8,11 @@ from rapticoressvc.storage_helpers.files_helper import save_to_json_file
 from rapticoressvc.svcc_constants import BUCKET_NAME
 from rapticoressvc.svcc_constants import STORAGE_S3
 
-os.environ["BUCKET_NAME"] = BUCKET_NAME
-os.environ["STORAGE_TYPE"] = STORAGE_S3
-os.environ["AWS_PROFILE"] = "dev1"  # be logged in to aws profile through sso
-os.environ["AWS_REGION"] = "us-west-2"
+# Uncomment before using this file
+# os.environ["BUCKET_NAME"] = BUCKET_NAME
+# os.environ["STORAGE_TYPE"] = STORAGE_S3
+# os.environ["AWS_PROFILE"] = "dev1"  # be logged in to aws profile through sso
+# os.environ["AWS_REGION"] = "us-west-2"
 
 """
 Generate NVD data for CVEs mentioned in sample_vulnerabilities_data.csv
